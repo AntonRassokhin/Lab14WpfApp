0581c40823d047fb86b4a5420ce782c5649e6a84
Rassokhin Anton <antonrassohin@gmail.com>
2022-01-14 15:07:48 +0300

    Добавьте файлы проекта.

---
+++ Lab14WpfApp_hw/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14WpfApp_hw
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<Product>();
+            products.Add(new Product()
+            { 
+                Name="Туалетная бумага",
+                Price=59,
+                ImagePath ="Data/1.png",
+                ProductType=ProductTypes.NonFood
+            });
+            products.Add(new Product()
+            {
+                Name = "Печенье Oreo",
+                Price = 149,
+                ImagePath = "Data/2.jpg",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Шоколад Аленка",
+                Price = 89,
+                ImagePath = "Data/3.jpg",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Жидкость для стекол -20",
+                Price = 249,
+                ImagePath = "Data/4.jpg",
+                ProductType = ProductTypes.NonFood
+            });
+            lstBox.ItemsSource = products;
+
+        }
+    }
+}
---
+++ Lab14WpfApp_hw/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14WpfApp_hw
+{
+    public enum ProductTypes
+    {
+        Food,
+        NonFood
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public int Price { get; set; }
+        public ProductTypes ProductType { get; set; }        
+    }
+}
